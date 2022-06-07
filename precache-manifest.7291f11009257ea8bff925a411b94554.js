03ae11c7d847a7ce33bced3a974b49fd9648fab1
Abhinav <abhinav.k@xcitech.in>
2022-06-07 18:48:45 +0530

    Updates

---
+++ precache-manifest.7291f11009257ea8bff925a411b94554.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "08c37c984c1fb644b14a",
+    "url": "/rajvan-fintech/static/css/main.b2f7343c.chunk.css"
+  },
+  {
+    "revision": "08c37c984c1fb644b14a",
+    "url": "/rajvan-fintech/static/js/main.08c37c98.chunk.js"
+  },
+  {
+    "revision": "8c98aab82769540efb62",
+    "url": "/rajvan-fintech/static/js/1.8c98aab8.chunk.js"
+  },
+  {
+    "revision": "f8d3cc5a2db7b2b01b5b",
+    "url": "/rajvan-fintech/static/js/2.f8d3cc5a.chunk.js"
+  },
+  {
+    "revision": "f4486894971b48940dec",
+    "url": "/rajvan-fintech/static/js/runtime~main.f4486894.js"
+  },
+  {
+    "revision": "21d88ba5d8ed598dd25e0c0cbfa8e182",
+    "url": "/rajvan-fintech/index.html"
+  }
+];
